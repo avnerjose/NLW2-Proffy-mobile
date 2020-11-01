f946149e54a3cddc012f0bad3f55d08d29619699
avnerjose <49520658+avnerjose@users.noreply.github.com>
2020-11-01 00:35:34 -0300

    Changing Routes

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -1,22 +1,44 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Landing from '../pages/Landing'
-import GiveClasses from '../pages/GiveClasses'
+import Landing from '../pages/Landing';
+import GiveClasses from '../pages/GiveClasses';
 import StudyTabs from './StudyTabs';
+import Login from '../pages/Login';
+import ForgotPasword from '../pages/ForgotPassword';
+import ConclusionScreen from '../pages/ConclusionScreen';
+import RegisterScreen1 from '../pages/Register/Screen1';
+import RegisterScreen2 from '../pages/Register/Screen2';
 
-const { Navigator, Screen } = createStackNavigator();
+export type RootStackParamList = {
+    Login: undefined,
+    ForgotPassword: undefined,
+    Register1: undefined,
+    Register2: undefined,
+    Conclusion: {
+        title: string,
+        subtitle: string,
+        label: string,
+    },
+    Landing: undefined,
+    GiveClasses: undefined,
+    StudyTabs: undefined,
+}
 
+const { Navigator, Screen } = createStackNavigator<RootStackParamList>();
 
-function AppStack() {
+const AppStack: React.FC = () => {
+
     return (
-        <NavigationContainer>
-            <Navigator screenOptions={{ headerShown: false }}>
-                <Screen name="Landing" component={Landing} />
-                <Screen name="GiveClasses" component={GiveClasses} />
-                <Screen name="StudyTabs" component={StudyTabs} />
-            </Navigator>
-        </NavigationContainer>
+        <Navigator screenOptions={{ headerShown: false }}>
+            <Screen name="Login" component={Login} />
+            <Screen name="ForgotPassword" component={ForgotPasword} />
+            <Screen name="Register1" component={RegisterScreen1} />
+            <Screen name="Register2" component={RegisterScreen2} />
+            <Screen name="Conclusion" component={ConclusionScreen} />
+            <Screen name="Landing" component={Landing} />
+            <Screen name="GiveClasses" component={GiveClasses} />
+            <Screen name="StudyTabs" component={StudyTabs} />
+        </Navigator>
     );
 }
 
